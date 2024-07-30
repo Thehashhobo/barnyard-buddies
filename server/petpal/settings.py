084320ed08be768baeb765fcabe39a368497eff7
Jerry Wang <79975526+thehoodhobo@users.noreply.github.com>
2024-07-30 19:57:15 -0400

    reconfigure to show media

--- server/petpal/settings.py
+++ server/petpal/settings.py
@@ -174,8 +174,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Uploaded files
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = "/media/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'server', 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
