2da7465b377d5e8576cd2038f32936681612f7d4
Jerry Wang <79975526+thehoodhobo@users.noreply.github.com>
2024-07-30 15:55:26 -0400

    Deploy Django backend to Heroku

--- server/petpal/wsgi.py
+++ server/petpal/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.petpal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'petpal.settings')
 
 application = get_wsgi_application()
