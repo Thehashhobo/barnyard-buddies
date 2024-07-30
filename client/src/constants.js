c5ca3519a99da577ee4c2c3873202c59059b0654
Jerry Wang <79975526+thehoodhobo@users.noreply.github.com>
2024-07-30 01:47:27 -0400

    added procfile and changed endpoints

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
 // constants.js
 export const SAMPLE_USER_ID = 1;
-export const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/";
+export const API_URL = import.meta.env.VITE_API_URL || "https://barnyard-buddies-e9ccf417b711.herokuapp.com/";
