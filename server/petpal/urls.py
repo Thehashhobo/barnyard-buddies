ccaf32abc0522f3830dea0c02b70786c76fe1188
Jerry Wang <79975526+thehoodhobo@users.noreply.github.com>
2024-07-30 19:35:54 -0400

    reconfigure to show media

--- server/petpal/urls.py
+++ server/petpal/urls.py
@@ -28,3 +28,9 @@ urlpatterns = [
     path('notifications/', include('notifications.urls')),
     path('', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
