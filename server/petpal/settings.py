5628a34261863128baa60b708dd937fbde4f2156
Jerry Wang <79975526+thehoodhobo@users.noreply.github.com>
2024-07-31 20:01:52 -0400

    use static files for prod

--- server/petpal/settings.py
+++ server/petpal/settings.py
@@ -158,10 +158,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'server/static'),
+    os.path.join(BASE_DIR, 'media'),
 ]
 django_heroku.settings(locals())
 
@@ -177,7 +175,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'server', 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 from datetime import timedelta
