05ba4dd873862c761295c1558005f27cea6e7e06
Jerry Wang <79975526+thehoodhobo@users.noreply.github.com>
2024-07-31 20:47:58 -0400

    change static url

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,7 @@
 // constants.js
 export const SAMPLE_USER_ID = 1;
 export const API_URL = import.meta.env.VITE_API_URL || "https://barnyard-buddies-e9ccf417b711.herokuapp.com/";
-const STATIC_URL = process.env.NODE_ENV === 'production' ? '/static/' : 'https://barnyard-buddies-e9ccf417b711.herokuapp.com/static/';
+export const STATIC_URL = process.env.NODE_ENV === 'production' 
+  ? 'https://barnyard-buddies-e9ccf417b711.herokuapp.com/static/' 
+  : '/static/';
 
